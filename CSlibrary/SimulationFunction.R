62a00e471e099a4aa4809786d5db18c5d3b279bd
jorgesicacha <39740183+jorgesicacha@users.noreply.github.com>
2021-04-21 11:05:42 +0200

    Add files via upload

--- CSlibrary/SimulationFunction.R
+++ CSlibrary/SimulationFunction.R
@@ -2,17 +2,33 @@
 ###########
 ## INPUT ##
 ###########
-
+setwd("/Users/jorgespa/Documents/Research/DataIntegration/DeadBirds")
+source("functionssimu.R")
 nspecies = 4
 
 ## For the species we're interested in (Species 1)
-input <- data.frame(
-  sigma2 = c(0.2, 1.2, 2, 0.1),
-  range = c(1.2, 2.5, 3.2, 0.22),
-  beta0 = c(0.8, 2.5, -1.5, 1.2),
-  betacov = c(1.5, -0.12, 2,-0.4),
-  alpha0 = c(2,-0.3, 5, 1.2),
-  alphacov = c(-2, -0.5, -2.5, 2)
+# input <- data.frame(
+#   sigma2 = c(0.2, 1.2, 2, 0.1),
+#   range = c(1.2, 2.5, 3.2, 0.22),
+#   beta0 = c(0.8, 2.5, -1.5, 1.2),
+#   betacov = c(1.5, -0.12, 2,-0.4),
+#   alpha0 = c(2,-0.3, 5, 1.2),
+#   alphacov = c(-2, -0.5, -2.5, 2)
+# )
+
+input <- list(
+  ecological = list(
+    fixed.effect=list(
+      intercept = c(0.8, 2.5, -1.5, 1.2),
+      betacov = c(1.5, -0.12, 2,-0.4)
+    ),
+    hyperparameters = list(
+      sigma2 = c(0.2, 1.2, 2, 0.1),
+      range = c(1.2, 2.5, 3.2, 0.22)
+    )
+  ),
+  sampling = list(),
+  detection = list()
 )
 
 thin_input <- list(
@@ -26,8 +42,8 @@ seed= 1036610602
 
 ## Covariates ##
 
-x0 <- seq(min(mesh$loc[, 1]), max(mesh$loc[, 1]), length = 100)
-y0 <- seq(min(mesh$loc[, 2]), max(mesh$loc[, 2]), length = 100)
+x0 <- seq(-1, 4, length = 100)
+y0 <- seq(-1,4, length = 100)
 gridlocs <- expand.grid(x0,y0)
 gridcov <- outer(x0, y0, function(x,y) cos(x) - sin(y - 2))
 covariate.im <- im(gridcov, x0, y0)
@@ -38,22 +54,24 @@ covariate_thin.im <- im(gridcov_thin, x0, y0)
 gridcov_det <- outer(x0, y0, function(x,y) (x/2)^2+(y/2)^2)
 covariate_detect.im <- im(gridcov_det, x0, y0)
 
+idxs <- list(eco=c(1),sampling=c(2),detection=c(3))
 
+cov <- list(covariate.im,covariate_thin.im,covariate_detect.im)
 
-
-csdata <- function(nspecies,input,thin_input,cov,domain=NULL,seed){
+csdata <- function(nspecies,input,thin_input,cov,idxs,domain=NULL,seed){
   
+  setwd("/Users/jorgespa/Documents/Research/DataIntegration/DeadBirds")
   source("functionssimu.R")
   set.seed(seed)
   RFoptions(seed=seed)
   
   
-  
+  domain <- NULL
   ## Where the simulation is performed ##
   if(is.null(domain)){
-  coordsmat <- matrix(c(0,0,3,0,3,3,0,3,0,0),ncol=2,byrow=T)
-  aa <- SpatialPolygons(list(Polygons(list(Polygon(coordsmat)),ID=1)))
-  win <- as.owin(aa)
+    coordsmat <- matrix(c(0,0,3,0,3,3,0,3,0,0),ncol=2,byrow=T)
+    aa <- SpatialPolygons(list(Polygons(list(Polygon(coordsmat)),ID=1)))
+    #win <- as.owin(aa) ##Need maptools
   }
   
   ## Mesh for models ##
@@ -65,7 +83,7 @@ csdata <- function(nspecies,input,thin_input,cov,domain=NULL,seed){
   
   if(is.list(cov)){
     
-    classes <- unique(lapply(cov, class))
+    classes <- unique(sapply(cov, class))
     if(length(classes)==1){
       
       if(classes=="im"){
@@ -82,19 +100,57 @@ csdata <- function(nspecies,input,thin_input,cov,domain=NULL,seed){
           covs.im <- lapply(cov,as.im)
           covs.raster <- lapply(cov, sppixels2raster)
           covs.sppixels <- cov
-          }
+        }
           else{
             stop("Covariates must be of 'im', 'RasterLayer' or 'SpatialPixelsDataFrame'")
           }
-          }}
+        }}
       
     }
     else{
       stop("All the covariate must be in the same format")
     }
-
     
-
+    #Indexes ##  
+    eco_idxs <- idxs$eco
+    sampling_idxs <- idxs$sampling
+    detect_idxs <- idxs$detection
+    
+    eco_covs.im <- covs.im[eco_idxs]
+    eco_covs.raster <- covs.raster[eco_idxs]
+    eco_covs.sppixels <- covs.sppixels[eco_idxs]
+    
+    sampling_covs.im <- covs.im[sampling_idxs]
+    sampling_covs.raster <- covs.raster[sampling_idxs]
+    sampling_covs.sppixels <- covs.sppixels[sampling_idxs]
+    
+    detect_covs.im <- covs.im[detect_idxs]
+    detect_covs.raster <- covs.raster[detect_idxs]
+    detect_covs.sppixels <- covs.sppixels[detect_idxs]
+    
+    ## Generate ecological process ##
+    
+    #Ecological Process
+    Eco_PP <- list()
+    
+    ## Bringing ecological covariates ##
+    p_eco <- length(eco_idxs)
+    eco_form <- c()
+    for(j in 1:p_eco){
+      eco_form[j] <- paste0("input$ecological$fixed.effect[[",j+1,"]][i]*eco_covs.im[[",j,"]]")
+    }
+    
+    eco_linpred <- paste(c("input$ecological$fixed.effect[[1]][i]",eco_form),collapse="+")
+    
+    for(i in 1:nspecies){
+      Eco_PP[[i]] <- rLGCP(model="matern",mu=eval(parse(text=eco_linpred)),
+                           var=input$ecological$hyperparameters[[1]][i],scale=input$ecological$hyperparameters[[2]][i]/sqrt(8),nu=1)
+      
+    }
+    
+    
+    
+    
   }
   else{
     stop("Covariates input should be a list")
