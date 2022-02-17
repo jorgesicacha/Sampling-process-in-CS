caa5867f68813e8ea8d96f443b3cf6b0ac338db7
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2022-02-17 07:40:01 +0100

    Update nimble_code.R

--- CSlibrary_new/nimble_code.R
+++ CSlibrary_new/nimble_code.R
@@ -1,5 +1,6 @@
 library(nimble)
-
+source("estpar.R")
+load("data_for_nimble.RData")
 
 code <-nimbleCode({
   #prior for omega
@@ -8,40 +9,68 @@ code <-nimbleCode({
     omega[i, 1:nspecies] ~ ddirch(alpha = alpha[1:nspecies])
   }
   
-  r[1:24] <- nimble_INLA(omega)
+  r[1:nsite,1:38] <- nimble_INLA(omega[1:nspecies,1:nspecies]) #change 38 to a constant to be specified
   
   for(i in 1:nsite){
-    for(j in 1:nspecies){
-    # lambda_obs[i,j] <- r[j%%]
-    }
+   # for(j in 1:nspecies){
+    lambda_obs[i,1] <- r[i,7] + r[i,8]*true_cov[i] + r[i,13]+ 
+      r[i, 9] + r[i,10]*bias_cov[i]+ r[i,14] - log(1+exp(r[i, 9] + r[i,10]*bias_cov[i]+ r[i,14])) + 
+      r[i, 11] + r[i,12]*det_cov[i] - log(1+exp(r[i, 11] + r[i,12]*det_cov[i]))  
+  # Second species
+    lambda_obs[i,2] <- r[i,15] + r[i,16]*true_cov[i] + r[i,21]+ 
+      r[i, 17] + r[i,18]*bias_cov[i]+ r[i,22] - log(1+exp(r[i, 17] + r[i,18]*bias_cov[i]+ r[i,22])) + 
+      r[i, 19] + r[i,20]*det_cov[i] - log(1+exp(r[i, 19] + r[i,20]*det_cov[i]))  
+    # Third species
+    lambda_obs[i,3] <- r[i,23] + r[i,24]*true_cov[i] + r[i,29]+ 
+      r[i, 25] + r[i,26]*bias_cov[i]+ r[i,30] - log(1+exp(r[i, 25] + r[i,26]*bias_cov[i]+ r[i,30])) + 
+      r[i, 27] + r[i,28]*det_cov[i] - log(1+exp(r[i, 27] + r[i,28]*det_cov[i])) 
+    # Third species
+    lambda_obs[i,4] <- r[i,31] + r[i,32]*true_cov[i] + r[i,37]+ 
+      r[i, 33] + r[i,34]*bias_cov[i]+ r[i,38] - log(1+exp(r[i, 33] + r[i,34]*bias_cov[i]+ r[i,38])) + 
+      r[i, 35] + r[i,36]*det_cov[i] - log(1+exp(r[i,35] + r[i,36]*det_cov[i]))  
+    
+    # }
   }
 
+  log(lambda[1:nsite, 1:nspecies]) <- lambda_obs[1:nsite, 1:nspecies]
   
-  inla.res[1:2] <- nimble_INLA(x_obs[1:N,1:2],y_obs[1:N],beta[1:2])
-  #for(j in 1:2){
-  beta[1] ~ dnorm(0,sd=1)  
-  beta[2] ~ dnorm(0,sd=1)
-  #}
-  for(i in 1:N){
-    linpred[i] <- inla.res[1]+ beta[1]*x[i,1]+ beta[2]*x[i,2]
-    y[i] ~ dnorm(linpred[i],inla.res[2])
+  #Proportion of lambdas
+  
+  
+  # Proportion for the multinomial distribution
+  for(site.tag in 1:nsite){
+    for(spe.tag in 1:nspecies){
+      prop[site.tag,spe.tag] <- (lambda[site.tag, spe.tag])/sum(lambda[site.tag, 1:nspecies])
+    }
   }
-  #Note that x=x_obs, y=y_obs
-  #I don't know if I am doing right
-  #But that is a trick I have employed
+  
+  
+  # True data
+  for(site.tag in 1:nsite){
+      C[site.tag] ~ dcat(prop[site.tag,1:nspecies])
+  }
+  
+  # Reported species
+    for(site.tag in 1:nsite){
+      Y[site.tag] ~ dcat(omega[C[site.tag],1:nspecies])
+    }
+  
 })
 
 
 ## Parameterising the nimble model
 
 #Data
-inla_data <- list(y_obs=df$y, 
-                  x_obs = df$x,
-                  y = df$y,
-                  x=df$x)
+inla_data <- list(Y=data_df$Y, 
+                  C = data_df$C,
+                true_cov = data_df$eco_cov,
+                  bias_cov=data_df$samp_cov,
+                det_cov= data_df$det_cov)
 
 #Constants
-const <- list(N = 100)
+const <- list(nspecies=length(unique(data_df$C)),
+              nsite = length(data_df$C)
+              )
 
 # Initial values
 idm_inits <- function(){list(beta=c(1,1)
@@ -72,7 +101,7 @@ mwtc <- nimbleModel(code,
 Cmwtc <- compileNimble(mwtc,showCompilerOutput = FALSE) #Have issues compiling
 
 
-mcmcconf <- configureMCMC(Cmwtc, monitors = c("beta", "inla.res"))
+mcmcconf <- configureMCMC(Cmwtc, monitors = c("omega"))
 
 Rmcmc <- buildMCMC(mcmcconf, 
                    enableWAIC =FALSE)
